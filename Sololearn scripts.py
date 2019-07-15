d2e385682f2b3731fd42c12fbae5d02cb7f30b34
NarutoProject <49729056+NarutoProject@users.noreply.github.com>
2019-07-15 18:42:56 +0300

    Файлы для обучения Python

--- Sololearn scripts.py
+++ Sololearn scripts.py
@@ -177,6 +177,7 @@ result = first * second
 print(result.x)
 print(result.y)
 
+
 # __sub__ - необходим для оператора -
 # __mul__ - для *
 # __truediv__ - для /
@@ -189,14 +190,16 @@ print(result.y)
 
 class SpecialString:
     def __init__(self, cont):
-        self.cont=cont
+        self.cont = cont
+
     def __truediv__(self, other):
         line = "=" * len(other.cont)
         return "\n".join([self.cont, line, other.cont])
 
+
 spam = SpecialString("Spam")
-hello= SpecialString("Hello!")
-print(spam/hello)
+hello = SpecialString("Hello!")
+print(spam / hello)
 # самостоятельный разбор join
 '''
 a="ТЫ"
@@ -206,6 +209,8 @@ shit=[a,b]
 c= "\n".join(shit)
 print(c)
 '''
+
+
 # магические методы для сравнения
 # __lt__ - для <
 # __le__ - для <=
@@ -213,4 +218,150 @@ print(c)
 # __ne__ - для != если не выполняется то возвращается противоположное __eq__
 # __gt__ - для >
 # __ge__ - для >=
+class Special_String:
+
+    def __init__(self, cont):
+        self.cont = cont
+
+    def __gt__(self, other):
+        for index in range(len(other.cont) + 1):
+            result = other.cont[:index] + ">" + self.cont
+            result += ">" + other.cont[index:]
+            print(result)
+
+
+spam = Special_String("Spam")
+eggs = Special_String("Eggs")
+spam > eggs
+
+
+# __len__ - для len()
+# __getitem__ - для индексации
+# __setitem__ - для присваивания значения индексированному элементу
+# __delitem__ - для удаления индексированных элементов
+# __iter__ - для перебора объектов(например в циклах for)
+# __contains__ - для in
+
+# Сокрытие данных
+# Условно частные методы и атрибуты оформляются с единым подчеркиванием в начале имени.
+# Это частные методы, которые не должны взаимодействовать со внешней частью программы.
+# Но часто это правило условно, внешняя часть программы может получить доступ к ним.
+# Реальная особенность этих методов лишь в том, что from module_name import * не будет импортировать переменные,
+# которые начинаются с единого подчеркивания.
+
+class Queue:
+    def __init__(self, contents):
+        self._hiddenlist = list(contents)
+
+    def push(self, value):
+        self._hiddenlist.insert(0, value)
+
+    def pop(self):
+        return self._hiddenlist.pop(-1)
+
+    def __repr__(self):
+        return "Queue({})".format(self._hiddenlist)
+
+
+queue = Queue([1, 2, 3])
+print(queue)
+queue.push(0)
+print(queue)
+queue.pop()
+print(queue)
+print(queue._hiddenlist)
+a = queue.pop()
+print(queue)
+print(a)
+
+
+# Строго частные методы и атрибуты оформляются с двойным подчеркиванием в начале имени.
+# Таким образом их имена искажаются, и внешняя часть программы не может получить к ним доступ.
+# Но это делается не для того, чтобы обеспечить их частность, а чтобы избежать ошибок,
+# если где-либо в коде есть подклассы, которые имеют методы или атрибуты с такими же именами.
+# Методы с искаженными именами все же могут быть вызваны извне, но по другим именам.
+# Метод __privatemethod класса Spam может быть вызван извне по имени _Spam__privatemethod
+class Spam:
+    __egg = 7
+
+    def print_egg(self):
+        print(self.__egg)
+
+
+s = Spam()
+s.print_egg()
+print(s._Spam__egg)
+
+
+# print(s.__egg)#error 'cause it's hidden method
+
+# Методы классов и статические методы
+# Методы объектов, рассмотренные до этого, вызываются экземпляром класса, который затем передается в параметр метода self.
+# Методы класса несколько другие: они вызываются классом, который передается параметру cls метода.
+# Чаще всего это используется в фабричных методах: создается экземпляр класса, при этом используются иные параметры, чем те,
+# которые обычно передаются в конструктор класса.
+# Методы класса оформляются с декоратором classmethod
+class Rectange:
+    def __init__(self, width, height):
+        self.width = width
+        self.height = height
+
+    def calculate_area(self):
+        return self.width * self.height
+
+    @classmethod
+    def new_square(cls, side_length):
+        return cls(side_length, side_length)
+
+
+square = Rectange.new_square(5)
+print(square.calculate_area())
+
+
+class Pizza:
+    def __init__(self, toppings):
+        self.toppings = toppings
+        self._pineapple_allowed= False
+    @staticmethod
+    def validate_topping(topping):
+        if topping == "pineapple":
+            raise ValueError("No pineapples!")
+        else:
+            return True
+
+    def what(self):
+        print("Your pizza constructored by: ", self.toppings)
+
+    @property #свойство
+    def pineapple_allowed(self):
+        return self._pineapple_allowed
+
+    @pineapple_allowed.setter
+    def pineapple_allowed(self,value):
+        if value:
+            password = input("Enter the password: ")
+            if password == "Swordfish":
+                self._pineapple_allowed=value
+            else:
+                raise ValueError("Alert! Intruder!")
+
+ingredients = ["cheese", "onions", "spam"]
+if all(Pizza.validate_topping(i) for i in ingredients):
+    pizza = Pizza(ingredients)
+
+pizza.what()
+print(pizza.pineapple_allowed)
+pizza.pineapple_allowed=True
+print(pizza.pineapple_allowed)
+
+# Свойства в свойствах мы можем настроить доступ к атрибутам экземпляра.
+# Чтобы создать свойства, непосредственно перед методом помещается декоратор property:
+# при вызове атрибута экземпляра с таким же именем, что и у метода, вместо него будет вызван метод.
+# Один из распространенных способов их применения - присвоение атрибуту свойства "только для чтения"
+
+# Свойства также могут быть заданы с помощью функций setter/getter.
+# Функция setter устанавливает значение для соответствующего свойства.
+# Функция getter возвращает значение.
+# Чтобы определить setter, используется декоратор с таким же именем, что и у свойства, с последующим ключевым словом setter, разделенные точкой.
+# Точно так же определяются функции getter.
 
